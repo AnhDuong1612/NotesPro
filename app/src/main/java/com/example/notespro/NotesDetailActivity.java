3486c845205e654252f4b7fea52ce9c50a340d79
LiliNguyen1612 <nnanhduong1612@gmail.com>
2023-05-02 16:39:18 +0700

    Save notes in Firebase

--- app/src/main/java/com/example/notespro/NotesDetailActivity.java
+++ app/src/main/java/com/example/notespro/NotesDetailActivity.java
@@ -1,10 +1,17 @@
 package com.example.notespro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+
 
 public class NotesDetailActivity extends AppCompatActivity {
 
@@ -25,13 +32,40 @@ public class NotesDetailActivity extends AppCompatActivity {
 
     void saveNote()
     {
-        String title = noteTitleEditText.getText().toString();
-        String content = noteContentEditText.getText().toString();
+        String noteTitle = noteTitleEditText.getText().toString();
+        String noteContent = noteContentEditText.getText().toString();
 
-        if( title==null || title.isEmpty())
+        if( noteTitle==null || noteTitle.isEmpty())
         {
             noteTitleEditText.setError("Title is required");
             return;
         }
+        Note note = new Note();
+        note.setTitle(noteTitle);
+        note.setContent(noteContent);
+        note.setTimestamp(Timestamp.now());
+
+        saveNoteToFirebase(note);
     }
+
+    void saveNoteToFirebase(Note note)
+    {
+        DocumentReference documentReference;
+        documentReference = Utility.getCollectionReferenceForNotes().document();
+        documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful())
+                {
+                    Utility.showToast(NotesDetailActivity.this,"Save note successful");
+                }
+                else
+                {
+                    Utility.showToast(NotesDetailActivity.this,"Failure , please check again");
+                }
+            }
+        });
+    }
+
+
 }
